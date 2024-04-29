fdecaafc0d07af8c7a6cf741e252ac3fc2fb03d0
acsandmann <asandmann@icloud.com>
2024-04-29 11:39:36 -0400

    404 + footer base

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CarList from './CarList';
 import CarDetail from './CarDetail';
+import Lost from './components/404Page';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
         <Routes>
           <Route path="/" element={<CarList />} />
           <Route path="/car/:sale_id" element={<CarDetail />} />
+          <Route path="*" element={<Lost />} />
         </Routes>
+        <Footer />
       </div>
     </Router>
   );
