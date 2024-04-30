b5ff8bff6069bec26d388464b38a5054f5236a96
sbalsara05 <balsarasohum@gmail.com>
2024-04-30 10:23:18 -0400

    Added some basic information to each of the car listings

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -13,6 +13,10 @@ const CarCard = ({ car, showModal, setModal, setCar }) => {
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{`${car.year} ${car.brand} ${car.model}`}</h5>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                     maybe something?
+                    Mileage: {car.miles !== null ? car.miles.toLocaleString() : "N/A"} miles <br />
+                    Transmission: {(car.transmission)} <br />
+                    Inspected: {(car.inspect)} <br/>
+                {/*    Basic details of each car */}
                 </p>
 
                 <button
